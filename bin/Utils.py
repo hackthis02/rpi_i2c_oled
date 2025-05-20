4a032e505646fd8d51abbf379bcc3067486bc58d
Timothy Massing <tim.massing@gmail.com>
2025-05-20 11:36:07 -0500

    Trying to fix regex warning.

--- bin/Utils.py
+++ bin/Utils.py
@@ -104,8 +104,8 @@ class HassioUtils(Utils):
         }
         text = Utils.compile_text(text, {**replacements, **additional_replacements})
         Utils.logger.info("compile_text:" + text)
-        regex = re.compile("{hassio\.[a-z]+\.[a-z\.]+}")
-        return regex.sub(lambda match: HassioUtils.get_hassio_info_property(match.string[match.start():match.end()][len("hassio\."):-1]), text)
+        regex = re.compile("{hassio\\.[a-z]+\\.[a-z\\.]+}")
+        return regex.sub(lambda match: HassioUtils.get_hassio_info_property(match.string[match.start():match.end()][len("hassio\\."):-1]), text)
 
     @staticmethod
     def get_hassio_info_property(properties_string):
