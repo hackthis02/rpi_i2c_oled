45fc6d304fc47bb4e87bdf78f3a68c87eaa76e5e
Timothy Massing <tim.massing@gmail.com>
2025-05-20 11:32:53 -0500

    Added logging

--- bin/Utils.py
+++ bin/Utils.py
@@ -103,6 +103,7 @@ class HassioUtils(Utils):
             "{ip}": lambda prop: HassioUtils.get_ip()
         }
         text = Utils.compile_text(text, {**replacements, **additional_replacements})
+        Utils.logger.info("compile_text:" + text)
         regex = re.compile("{hassio\.[a-z]+\.[a-z\.]+}")
         return regex.sub(lambda match: HassioUtils.get_hassio_info_property(match.string[match.start():match.end()][len("hassio\."):-1]), text)
 
